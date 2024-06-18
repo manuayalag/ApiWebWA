1a1b44779020f480f44df4536bb0cb4ba2244ff8
manuayalag <136390439+manuayalag@users.noreply.github.com>
2024-06-18 10:59:25 -0400

    Add files via upload

---
+++ Controllers/ClienteControl.cs
@@ -0,0 +1,58 @@
+﻿//ClienteControl.cs
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+using System.Net.Http.Headers;
+using WebApiWA.Models;
+
+namespace ApiWebWA.Controllers
+{
+    public class ClienteControl : ControllerBase
+    {
+        //RECIBIMOS LOS DATOS DE VALIDACION VIA GET
+        [HttpGet]
+        //DENTRO DE LA RUTA webhook
+        [Route("webhook")]
+        //RECIBIMOS LOS PARAMETROS QUE NOS ENVIA WHATSAPP PARA VALIDAR NUESTRA URL
+        public string Webhook(
+            [FromQuery(Name = "hub.mode")] string mode,
+            [FromQuery(Name = "hub.challenge")] string challenge,
+            [FromQuery(Name = "hub.verify_token")] string verify_token
+        )
+        {
+            //SI EL TOKEN ES hola (O EL QUE COLOQUEMOS EN FACEBOOK)
+            if (verify_token.Equals("qaz123wsx"))
+            {
+                return challenge;
+            }
+            else
+            {
+                return "";
+            }
+        }
+        //RECIBIMOS LOS DATOS DE VIA POST
+        [HttpPost]
+        //DENTRO DE LA RUTA webhook
+        [Route("webhook")]
+        //RECIBIMOS LOS DATOS Y LOS GUARDAMOS EN EL MODELO WebHookResponseModel
+        public dynamic datos([FromBody] WebHookResponseModel entry)
+        {
+            //ESTRAEMOS EL MENSAJE RECIBIDO
+            string mensaje_recibido = entry.entry[0].changes[0].value.messages[0].text.body;
+            //ESTRAEMOS EL ID UNICO DEL MENSAJE
+            string id_wa = entry.entry[0].changes[0].value.messages[0].id;
+            //ESTRAEMOS EL NUMERO DE TELEFONO DEL CUAL RECIBIMOS EL MENSAJE
+            string telefono_wa = entry.entry[0].changes[0].value.messages[0].from;
+            //INICIALIZAMOS LA CONEXION A LA BD
+            Datos dat = new Datos();
+            //INSERTAMOS LOS DATOS RECIBIDOS
+            dat.insertar(mensaje_recibido, id_wa, telefono_wa);
+            //SI NO HAY ERROR RETORNAMOS UN OK
+            var response = new HttpResponseMessage(HttpStatusCode.OK);
+            response.Content.Headers.ContentType = new MediaTypeHeaderValue("text/plain");
+            return response;
+
+
+        }
+
+    }
+}
---
+++ Models/Datos.cs
@@ -0,0 +1,65 @@
+﻿//Recibir mensajes con Api Oficial de WhatsApp en C#
+namespace WebApiWA.Models
+{
+    using System.Data.SqlClient;
+    public class Datos
+    {
+        public void insertar(string mensaje_recibido, string id_wa, string telefono_wa)
+        {
+            var connection = new SqlConnection("Server=localhost;Database=master;Trusted_Connection=True;");
+            try
+            {
+                var command = connection.CreateCommand();
+                command.CommandText = "INSERT INTO aspwa.dbo.registro` (`mensaje_recibido`, `id_wa`, `telefono_wa`) VALUES ('" + mensaje_recibido + "', '" + id_wa + "', '" + telefono_wa + "');";
+                connection.Open();
+                command.ExecuteNonQuery();
+                //return "Mitarbeiter wurde angelegt";
+            }
+            catch (Exception ex)
+            {
+                //return ex.Message;
+            }
+            finally
+            {
+                connection.Close();
+            }
+        }
+
+    }
+
+    public class WebHookResponseModel
+    {
+        public Entry[] entry { get; set; }
+    }
+
+    public class Entry
+    {
+        public Change[] changes { get; set; }
+    }
+
+    public class Change
+    {
+        public Value value { get; set; }
+    }
+
+    public class Value
+    {
+        public int ad_id { get; set; }
+        public long form_id { get; set; }
+        public long leadgen_id { get; set; }
+        public int created_time { get; set; }
+        public long page_id { get; set; }
+        public int adgroup_id { get; set; }
+        public Messages[] messages { get; set; }
+    }
+    public class Messages
+    {
+        public string id { get; set; }
+        public string from { get; set; }
+        public Text text { get; set; }
+    }
+    public class Text
+    {
+        public string body { get; set; }
+    }
+}
